ec539f0697a85534635c7fcd39f6ec0d8899efdd
GG <46346356+GoonertheGreat1@users.noreply.github.com>
2023-09-12 16:55:37 +0100

    Update main.tf again after fix
    fixing the bucket names

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "google_storage_bucket" "gcs_bucket" {
+resource "google_storage_bucket" "gcs_bucket1" {
   name = "test-bucket-random-001123"
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
+resource "google_storage_bucket" "gcs_bucket2" {
   name = "test-bucket-random-99999"
 }
